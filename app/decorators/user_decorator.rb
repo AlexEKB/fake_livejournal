779bba4833eac1eed03e68874f91090e680ee0d7
alexekb <pu4kom@mail.ru>
2019-04-11 17:58:41 +0500

    user/show ready

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,9 +1,13 @@
 class UserDecorator < BaseDecorator
   include Draper::LazyHelpers
-  delegate :email, :id
+  delegate :email, :id, :first_name, :last_name
 
   def username
     "#{email.split('@')[0].capitalize}"
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
   end
 
   def email_with_id
