3da1a1de93074544d1a6bfe54af8d630f2f7b66c
alexekb <pu4kom@mail.ru>
2019-04-13 13:04:53 +0500

    gem bonsai-elasticsearch-rails added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
 end
